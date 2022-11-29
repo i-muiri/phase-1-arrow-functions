36ab339f910c2cea3bbe233dabd9ff8746383cdf
i-muiri <ianmuiri99@gmail.com>
2022-11-29 15:20:17 +0300

    Completed Assignemt

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(){
+    const num1=2000, num2=100
+    const ans = num1 / num2
+    return ans
+}
+
+square = (num) => {
+return num * num
+}
+
+add = (val1, val2) => {
+    return val1 + val2
+}
